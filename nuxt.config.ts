c9aac2cd15f6c4518b98b6c2bde1ec669f8cb7ef
feb19 <nt@feb19.jp>
2023-11-14 22:07:51 +0900

    sample

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,17 @@
 import { defineNuxtConfig } from "nuxt/config"
 
 export default defineNuxtConfig({
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-    ]
+  app: {
+    head: {
+      title: process.env.npm_package_name || '',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      ],
+      link: [
+      ]
+    },
   },
   modules: [
     "@nuxt/content",
